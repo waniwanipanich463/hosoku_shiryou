aa885611b2571ca61b48ad06a3b8d24ac8180e68
庭野翔太 <niwanoshouta@niwanoshoutanoMacBook-Pro.local>
2026-03-31 01:12:44 +0900

    test: deploy in verification mode (18:00 restriction off)

--- main.js
+++ main.js
@@ -4,14 +4,14 @@ const videoData = [
         "title": "【考察総集編】アメリカで何が起きた!?スタバ閉店の連鎖で米国経済完全終了",
         "thumbnail": "https://img.youtube.com/vi/cnTFi9G64BM/maxresdefault.jpg",
         "description": "最新の動画内容についてはYouTubeでご確認ください。",
-        "downloadUrl": "./downloads/cnTFi9G64BM.pdf"
+        "downloadUrl": "https://acrobat.adobe.com/id/urn:aaid:sc:AP:343f27d1-295a-4c36-a925-b05516d38224"
     },
     {
         "id": "O4I213UZYQY",
         "title": "【考察総集編】医療費14,000ドルでも長生きできないアメリカ!?米国医療システム破綻で国民に与える影響を考察します！",
         "thumbnail": "https://img.youtube.com/vi/O4I213UZYQY/maxresdefault.jpg",
         "description": "最新の動画内容についてはYouTubeでご確認ください。",
-        "downloadUrl": "./downloads/O4I213UZYQY.pdf"
+        "downloadUrl": "https://acrobat.adobe.com/id/urn:aaid:sc:AP:343f27d1-295a-4c36-a925-b05516d38224"
     }
 ];
 
@@ -19,10 +19,10 @@ function createVideoCard(video) {
     const card = document.createElement('article');
     card.className = 'card';
     
-    // 18時制限のチェック
+    // 18時制限のチェック (【検証用】一時的に常にtrueにしてデプロイします)
     const now = new Date();
     const currentHour = now.getHours();
-    const isAfter18 = currentHour >= 18;
+    const isAfter18 = true; 
 
     let downloadLabel = "COMING SOON";
     let downloadClass = "download-btn disabled";
